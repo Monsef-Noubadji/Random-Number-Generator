92abf95ebcbb2ef603c4252df276b9393eba5b82
monsef noubadji <noubadjimonsef@gmail.com>
2021-08-02 12:54:06 +0100

    javascript source code

---
+++ generate.js
@@ -0,0 +1,13 @@
+// let H = 21;
+// let B = 20;
+// console.log(`\_${H}\_${B}${H}\_${B}${H}\_${B}${H}\_${B}`); //_21_2021_2021_2021_20
+// let userID = Window.prompt("Enter the name to thank :)")
+// console.log(`Thank you ${userID} :)`)
+
+function generateNum(){ 
+let myNumber = 90, elNumber = myNumber + Math.floor(Math.random() * (myNumber * myNumber));
+alert(`"Your number is :${elNumber + myNumber + (34 / 55 + 100,5)} "`);
+return 0;
+// console.log(myNumber);
+// console.log(elNumber);
+}
